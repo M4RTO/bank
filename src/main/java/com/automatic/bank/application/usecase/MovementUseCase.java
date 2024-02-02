16fd7e49c669a8452fd930871f53e93786af59e0
martin <martin.britez@gmail.com>
2024-02-02 17:12:45 +0100

    add readme and dockerFile with dockerCompose

--- src/main/java/com/automatic/bank/application/usecase/MovementUseCase.java
+++ src/main/java/com/automatic/bank/application/usecase/MovementUseCase.java
@@ -4,6 +4,7 @@ import com.automatic.bank.application.port.in.GetMovementsPortIn;
 import com.automatic.bank.application.port.out.GetMovementsPortOut;
 import com.automatic.bank.domain.Movement;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,7 +14,13 @@ import java.util.List;
 @Slf4j
 public class MovementUseCase implements GetMovementsPortIn {
 
-    private GetMovementsPortOut getMovementsPortOut;
+    private final GetMovementsPortOut getMovementsPortOut;
+
+    @Autowired
+    public MovementUseCase(GetMovementsPortOut getMovementsPortOut) {
+        this.getMovementsPortOut = getMovementsPortOut;
+    }
+
     @Override
     public List<Movement> execute(Long accountId, String type) {
         return getMovementsPortOut.findAllByAccountIdAndMovementType(accountId, type);
